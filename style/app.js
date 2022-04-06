137b9e5f4a56f21da830932c3622cabcf0fe5fc7
BravoDiego <101215860+BravoDiego@users.noreply.github.com>
2022-04-06 13:35:46 +0200

    Add files via upload

---
+++ style/app.js
@@ -0,0 +1,56 @@
+var add1 = document.getElementById('add1');
+var add2 = document.getElementById('add2');
+
+var remove1 = document.getElementById('remove1');
+var remove2 = document.getElementById('remove2');
+
+var compteur1 = document.getElementById('compteur1');
+var compteur2 = document.getElementById('compteur2');
+
+var total = document.getElementById('total');
+
+var compte = 1;
+var compte2 = 1;
+
+compteur1.textContent = compte;
+
+
+
+add1.addEventListener("click", plus1);
+add2.addEventListener("click", plus2);
+
+remove1.addEventListener("click", moin1);
+remove2.addEventListener("click", moin2);
+
+function totalcalcul() {
+    total.textContent = Math.round((54.99 * compte + 54.99 * compte2 + 19) * 100)/100;
+
+}
+
+function plus1() {
+    compte++;
+    compteur1.textContent = compte;    
+    totalcalcul();
+}
+
+function plus2() {
+    compte2++;
+    compteur2.textContent = compte2;    
+    totalcalcul();
+}
+
+function moin1() {
+    if(compte >= 1) {
+    compte--;
+    compteur1.textContent = compte;   
+    totalcalcul();
+    }
+}
+
+function moin2() {
+    if(compte2 >= 1) {
+    compte2--;
+    compteur2.textContent = compte2;    
+    totalcalcul();
+    }
+}
